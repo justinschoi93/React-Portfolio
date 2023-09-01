bf4fa0fbefd4e13f69bd794e6558f3eaaa671474
Justin 석원 Choi <justinschoi93@gmail.com>
2023-08-31 21:55:59 -0700

    Resume page framework set

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,3 +1,8 @@
 export default function Resume () {
-    return(<div>This is where my resume will go...</div>)
+    return(
+        <div>
+            <div id="resume">Resume</div>
+            <a download="file.path">Download</a>
+        </div>
+    )
 }
